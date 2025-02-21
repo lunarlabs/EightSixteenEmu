ced4dcbbd683770a1986df418e559093e6f6d0c3
Matthias <aussieevil@hotmail.com>
2025-02-21 12:22:09 -0800

    adding test project

---
+++ EightSixteenEmuTests/MicroprocessorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EightSixteenEmu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightSixteenEmu.Tests
+{
+    [TestClass()]
+    public class MicroprocessorTests
+    {
+        [TestMethod()]
+        public void MicroprocessorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
