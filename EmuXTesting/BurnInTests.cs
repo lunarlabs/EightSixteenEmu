122e4ae9a4a55a4ee82e49603d4f2c01bacc7bca
Matthias <aussieevil@hotmail.com>
2025-04-04 00:34:42 -0700

    Remove SingleStepGauntlet and add BurnInTests
    This commit removes the `SingleStepGauntlet.cs` file along with its associated classes. A new `BurnInTests.cs` file is introduced, containing the `BurnInTests` class with methods for quick and full burn-in testing. Additionally, `QuickBurnInData` and `BurnInTestState` classes are added to support the new testing framework.

--- EmuXTesting/BurnInTests.cs
+++ EmuXTesting/BurnInTests.cs
@@ -3,18 +3,26 @@ using System.Data;
 
 namespace EmuXTesting
 {
-    public class SingleStepGauntlet
+    public class BurnInTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [ClassData(typeof(QuickBurnInData))]
+        public void QuickBurnIn(BurnInTestState start, BurnInTestState goal, int cycles)
         {
 
+        }
+
+        [Theory]
+        public void FullBurnIn()
+        {
+            // this is going to execute 5,120,000 instructions. God help me.
+            // should I have the classdata be by file?
         }
 
 
     }
 
-    public class GauntletTestData : TheoryData<GauntletTestState, GauntletTestState, int>
+    public class QuickBurnInData : TheoryData<BurnInTestState, BurnInTestState, int>
     {
         /* Goal: After setting up the emulator with the first GauntletTestState, one instruction is run.
          * The state of the emulator is then compared to the second GauntletTestState, and the cycles counter is compared to the int.
@@ -29,7 +37,7 @@ namespace EmuXTesting
          */
     }
 
-    public class GauntletTestState(Microprocessor.MicroprocessorState state, Dictionary<uint, uint> ramValues)
+    public class BurnInTestState(Microprocessor.MicroprocessorState state, Dictionary<uint, uint> ramValues)
     {
         public readonly Microprocessor.MicroprocessorState State = state;
         public readonly Dictionary<uint, uint> RamValues = ramValues;
